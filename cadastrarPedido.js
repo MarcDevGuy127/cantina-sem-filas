d38b2e50432819c9cbcc5942bdb6438a459a921b
MarcDevGuy127 <marceloh.github@gmail.com>
2026-09-02 22:08:51 -0300

    refac(js): refactoring cadastrarPedido()

--- cadastrarPedido.js
+++ cadastrarPedido.js
@@ -3,12 +3,12 @@ function cadastrarPedido() {
     const quantidade = 2;
     const preco = 25;
 
-    const valorTtotal = quantidade * preco;
+    const valorTotal = quantidade * preco;
 
     console.log("Pedido cadastrado com sucesso!");
     console.log("Produto:", produto);
     console.log("Quantidade:", quantidade);
-    console.log("Total: R$ " + total);
+    console.log("Total: R$ " + valorTotal);
 }
 
 cadastrarPedido();
